7f2ae01d2ce88d51dff13d30b3238177c18489f4
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2020-07-18 02:16:40 +0700

    update +++

--- c2runtime.js
+++ c2runtime.js
@@ -25306,6 +25306,430 @@ cr.behaviors.Sin = function(runtime)
 }());
 ;
 ;
+cr.behaviors.Turret = function(runtime)
+{
+	this.runtime = runtime;
+};
+(function ()
+{
+	var behaviorProto = cr.behaviors.Turret.prototype;
+	behaviorProto.Type = function(behavior, objtype)
+	{
+		this.behavior = behavior;
+		this.objtype = objtype;
+		this.runtime = behavior.runtime;
+	};
+	var behtypeProto = behaviorProto.Type.prototype;
+	behtypeProto.onCreate = function()
+	{
+		this.targetTypes = [];						// object types to check for as targets
+	};
+	behaviorProto.Instance = function(type, inst)
+	{
+		this.type = type;
+		this.behavior = type.behavior;
+		this.inst = inst;				// associated object instance to modify
+		this.runtime = type.runtime;
+	};
+	var behinstProto = behaviorProto.Instance.prototype;
+	behinstProto.onCreate = function()
+	{
+		this.range = this.properties[0];
+		this.rateOfFire = this.properties[1];
+		this.rotateEnabled = (this.properties[2] !== 0);
+		this.rotateSpeed = cr.to_radians(this.properties[3]);
+		this.targetMode = this.properties[4];		// 0 = first, 1 = nearest
+		this.predictiveAim = (this.properties[5] !== 0);
+		this.projectileSpeed = this.properties[6];
+		this.enabled = (this.properties[7] !== 0);
+		this.useCollisionCells = (this.properties[8] !== 0);
+		this.lastCheckTime = 0;						// last time checked for targets in range
+		this.fireTimeCount = this.rateOfFire;		// counts up to rate of fire before shooting. starts in fully reloaded state
+		this.currentTarget = null;					// current target object
+		this.loadTargetUid = -1;
+		this.oldTargetX = 0;
+		this.oldTargetY = 0;
+		this.lastSpeeds = [0, 0, 0, 0];
+		this.speedsCount = 0;
+		this.firstTickWithTarget = true;
+		var self = this;
+		if (!this.recycled)
+		{
+			this.myDestroyCallback = function(inst) {
+										self.onInstanceDestroyed(inst);
+									};
+		}
+		this.runtime.addDestroyCallback(this.myDestroyCallback);
+	};
+	behinstProto.saveToJSON = function ()
+	{
+		var o = {
+			"r": this.range,
+			"rof": this.rateOfFire,
+			"re": this.rotateEnabled,
+			"rs": this.rotateSpeed,
+			"tm": this.targetMode,
+			"pa": this.predictiveAim,
+			"ps": this.projectileSpeed,
+			"en": this.enabled,
+			"lct": this.lastCheckTime,
+			"ftc": this.fireTimeCount,
+			"target": (this.currentTarget ? this.currentTarget.uid : -1),
+			"ox": this.oldTargetX,
+			"oy": this.oldTargetY,
+			"ls": this.lastSpeeds,
+			"sc": this.speedsCount,
+			"targs": []
+		};
+		var i, len;
+		for (i = 0, len = this.type.targetTypes.length; i < len; i++)
+		{
+			o["targs"].push(this.type.targetTypes[i].sid);
+		}
+		return o;
+	};
+	behinstProto.loadFromJSON = function (o)
+	{
+		this.range = o["r"];
+		this.rateOfFire = o["rof"];
+		this.rotateEnabled = o["re"];
+		this.rotateSpeed = o["rs"];
+		this.targetMode = o["tm"];
+		this.predictiveAim = o["pa"];
+		this.projectileSpeed = o["ps"];
+		this.enabled = o["en"];
+		this.lastCheckTime = o["lct"];
+		this.fireTimeCount = o["ftc"] || 0;		// not in <r154
+		this.loadTargetUid = o["target"];
+		this.oldTargetX = o["ox"];
+		this.oldTargetY = o["oy"];
+		this.lastSpeeds = o["ls"];
+		this.speedsCount = o["sc"];
+		cr.clearArray(this.type.targetTypes);
+		var i, len, t;
+		for (i = 0, len = o["targs"].length; i < len; i++)
+		{
+			t = this.runtime.getObjectTypeBySid(o["targs"][i]);
+			if (t)
+				this.type.targetTypes.push(t);
+		}
+	};
+	behinstProto.afterLoad = function ()
+	{
+		if (this.loadTargetUid === -1)
+			this.currentTarget = null;
+		else
+			this.currentTarget = this.runtime.getObjectByUID(this.loadTargetUid);
+	};
+	behinstProto.onInstanceDestroyed = function (inst)
+	{
+		if (this.currentTarget == inst)
+			this.currentTarget = null;
+	};
+	behinstProto.onDestroy = function ()
+	{
+		this.currentTarget = null;
+		this.runtime.removeDestroyCallback(this.myDestroyCallback);
+	};
+	behinstProto.addSpeed = function (s)
+	{
+		if (this.speedsCount < 4)
+		{
+			this.lastSpeeds[this.speedsCount] = s;
+			this.speedsCount++;
+		}
+		else
+		{
+			this.lastSpeeds.shift();
+			this.lastSpeeds.push(s);
+		}
+	};
+	behinstProto.getSpeed = function ()
+	{
+		var ret = 0;
+		var i = 0;
+		for ( ; i < this.speedsCount; i++)
+		{
+			ret += this.lastSpeeds[i];
+		}
+		return ret / this.speedsCount;
+	};
+	behinstProto.isInRange = function (obj_)
+	{
+		var inst = this.inst;
+		var dx = obj_.x - inst.x;
+		var dy = obj_.y - inst.y;
+		return dx * dx + dy * dy <= this.range * this.range;
+	};
+	var tmpRect = new cr.rect(0, 0, 0, 0);
+	var candidates = [];
+	behinstProto.lookForFirstTarget = function ()
+	{
+		var i, len, rinst;
+		tmpRect.left = this.inst.x - this.range;
+		tmpRect.top = this.inst.y - this.range;
+		tmpRect.right = this.inst.x + this.range;
+		tmpRect.bottom = this.inst.y + this.range;
+		if (this.useCollisionCells)
+		{
+			this.runtime.getTypesCollisionCandidates(null, this.type.targetTypes, tmpRect, candidates);
+		}
+		else
+		{
+			for (i = 0, len = this.type.targetTypes.length; i < len; ++i)
+			{
+				cr.appendArray(candidates, this.type.targetTypes[i].instances);
+			}
+		}
+		for (i = 0, len = candidates.length; i < len; ++i)
+		{
+			rinst = candidates[i];
+			if (this.isInRange(rinst))
+			{
+				this.currentTarget = rinst;
+				cr.clearArray(candidates);
+				return;
+			}
+		}
+		cr.clearArray(candidates);
+	};
+	behinstProto.lookForNearestTarget = function ()
+	{
+		var i, len, rinst, dist, dx, dy;
+		var myx = this.inst.x;
+		var myy = this.inst.y;
+		var closest = this.range * this.range;
+		this.currentTarget = null;
+		tmpRect.left = myx - this.range;
+		tmpRect.top = myy - this.range;
+		tmpRect.right = myx + this.range;
+		tmpRect.bottom = myy + this.range;
+		if (this.useCollisionCells)
+		{
+			this.runtime.getTypesCollisionCandidates(null, this.type.targetTypes, tmpRect, candidates);
+		}
+		else
+		{
+			for (i = 0, len = this.type.targetTypes.length; i < len; ++i)
+			{
+				cr.appendArray(candidates, this.type.targetTypes[i].instances);
+			}
+		}
+		for (i = 0, len = candidates.length; i < len; ++i)
+		{
+			rinst = candidates[i];
+			dx = myx - rinst.x;
+			dy = myy - rinst.y;
+			dist = dx * dx + dy * dy;
+			if (dist < closest)
+			{
+				this.currentTarget = rinst;
+				closest = dist;
+			}
+		}
+		candidates.length = 0;
+	};
+	behinstProto.tick = function ()
+	{
+		var dt = this.runtime.getDt(this.inst);
+		var nowtime = this.runtime.kahanTime.sum;
+		var inst = this.inst;
+		if (!this.enabled)
+			return;
+		if (this.currentTarget && !this.isInRange(this.currentTarget))
+		{
+			this.currentTarget = null;
+			this.speedsCount = 0;
+			this.firstTickWithTarget = true;
+		}
+		if (nowtime >= this.lastCheckTime + 0.1)
+		{
+			this.lastCheckTime = nowtime;
+			if (this.targetMode === 0 && !this.currentTarget)
+			{
+				this.lookForFirstTarget();
+				if (this.currentTarget)
+				{
+					this.speedsCount = 0;
+					this.firstTickWithTarget = true;
+					this.oldTargetX = this.currentTarget.x;
+					this.oldTargetY = this.currentTarget.y;
+					this.runtime.trigger(cr.behaviors.Turret.prototype.cnds.OnTargetAcquired, this.inst);
+				}
+			}
+			else if (this.targetMode === 1)
+			{
+				var oldTarget = this.currentTarget;
+				this.lookForNearestTarget();
+				if (this.currentTarget && this.currentTarget !== oldTarget)
+				{
+					this.speedsCount = 0;
+					this.firstTickWithTarget = true;
+					this.oldTargetX = this.currentTarget.x;
+					this.oldTargetY = this.currentTarget.y;
+					this.runtime.trigger(cr.behaviors.Turret.prototype.cnds.OnTargetAcquired, this.inst);
+				}
+			}
+		}
+		this.fireTimeCount += dt;
+		if (this.currentTarget)
+		{
+			var targetAngle = cr.angleTo(inst.x, inst.y, this.currentTarget.x, this.currentTarget.y);
+			if (this.predictiveAim)
+			{
+				var Gx = inst.x;
+				var Gy = inst.y;
+				var Px = this.currentTarget.x;
+				var Py = this.currentTarget.y;
+				var h = cr.angleTo(Px, Py, this.oldTargetX, this.oldTargetY);
+				if (!this.firstTickWithTarget)
+					this.addSpeed(cr.distanceTo(Px, Py, this.oldTargetX, this.oldTargetY) / dt);
+				var s = this.getSpeed();
+				var q = Py - Gy;
+				var r = Px - Gx;
+				var w = (s * Math.sin(h) * (Gx - Px) - s * Math.cos(h) * (Gy - Py)) / this.projectileSpeed;
+				var a = (Math.asin(w / Math.sqrt(q * q + r * r)) - Math.atan2(q, -r)) + Math.PI;
+				if (!isNaN(a))
+					targetAngle = a;
+			}
+			if (this.rotateEnabled)
+			{
+				inst.angle = cr.angleRotate(inst.angle, targetAngle, this.rotateSpeed * dt);
+				inst.set_bbox_changed();
+			}
+			if ((this.fireTimeCount >= this.rateOfFire) &&
+				(!this.rotateEnabled || cr.to_degrees(cr.angleDiff(inst.angle, targetAngle)) <= 0.1) &&
+				(!this.predictiveAim || this.speedsCount >= 4))
+			{
+				this.fireTimeCount -= this.rateOfFire;
+				if (this.fireTimeCount >= this.rateOfFire)
+					this.fireTimeCount = 0;
+				this.runtime.trigger(cr.behaviors.Turret.prototype.cnds.OnShoot, this.inst);
+			}
+			if (this.currentTarget)
+			{
+				this.oldTargetX = this.currentTarget.x;
+				this.oldTargetY = this.currentTarget.y;
+			}
+			this.firstTickWithTarget = false;
+		}
+		if (this.fireTimeCount > this.rateOfFire)
+			this.fireTimeCount = this.rateOfFire;
+	};
+	function Cnds() {};
+	Cnds.prototype.HasTarget = function ()
+	{
+		return !!this.currentTarget;
+	};
+	Cnds.prototype.OnShoot = function ()
+	{
+		return true;
+	};
+	Cnds.prototype.OnTargetAcquired = function ()
+	{
+		return true;
+	};
+	behaviorProto.cnds = new Cnds();
+	function Acts() {};
+	Acts.prototype.AcquireTarget = function (obj_)
+	{
+		if (!obj_)
+			return;
+		var instances = obj_.getCurrentSol().getObjects();
+		var i, len, inst;
+		for (i = 0, len = instances.length; i < len; ++i)
+		{
+			inst = instances[i];
+			if (this.currentTarget !== inst && this.isInRange(inst))
+			{
+				this.currentTarget = inst;
+				this.speedsCount = 0;
+				this.firstTickWithTarget = true;
+				this.oldTargetX = this.currentTarget.x;
+				this.oldTargetY = this.currentTarget.y;
+				this.runtime.trigger(cr.behaviors.Turret.prototype.cnds.OnTargetAcquired, this.inst);
+				break;
+			}
+		}
+	};
+	Acts.prototype.AddTarget = function (obj_)
+	{
+		var targetTypes = this.type.targetTypes;
+		if (targetTypes.indexOf(obj_) !== -1)
+			return;
+		var i, len, t;
+		for (i = 0, len = targetTypes.length; i < len; i++)
+		{
+			t = targetTypes[i];
+			if (t.is_family && t.members.indexOf(obj_) !== -1)
+				return;
+		}
+		targetTypes.push(obj_);
+	};
+	Acts.prototype.ClearTargets = function ()
+	{
+		cr.clearArray(this.type.targetTypes);
+	};
+	Acts.prototype.UnacquireTarget = function ()
+	{
+		this.currentTarget = null;
+		this.speedsCount = 0;
+		this.firstTickWithTarget = true;
+	};
+	Acts.prototype.SetEnabled = function (e)
+	{
+		this.enabled = (e !== 0);
+	};
+	Acts.prototype.SetRange = function (r)
+	{
+		this.range = r;
+	};
+	Acts.prototype.SetRateOfFire = function (r)
+	{
+		this.rateOfFire = r;
+	};
+	Acts.prototype.SetRotate = function (r)
+	{
+		this.rotateEnabled = (r !== 0);
+	};
+	Acts.prototype.SetRotateSpeed = function (r)
+	{
+		this.rotateSpeed = cr.to_radians(r);
+	};
+	Acts.prototype.SetTargetMode = function (s)
+	{
+		this.targetMode = s;
+	};
+	Acts.prototype.SetPredictiveAim = function (s)
+	{
+		this.predictiveAim = (s !== 0);
+	};
+	Acts.prototype.SetProjectileSpeed = function (s)
+	{
+		this.projectileSpeed = s;
+	};
+	behaviorProto.acts = new Acts();
+	function Exps() {};
+	Exps.prototype.TargetUID = function (ret)
+	{
+		ret.set_int(this.currentTarget ? this.currentTarget.uid : 0);
+	};
+	Exps.prototype.Range = function (ret)
+	{
+		ret.set_float(this.range);
+	};
+	Exps.prototype.RateOfFire = function (ret)
+	{
+		ret.set_float(this.rateOfFire);
+	};
+	Exps.prototype.RotateSpeed = function (ret)
+	{
+		ret.set_float(cr.to_degrees(this.rotateSpeed));
+	};
+	behaviorProto.exps = new Exps();
+}());
+;
+;
 cr.behaviors.jumpthru = function(runtime)
 {
 	this.runtime = runtime;
@@ -26498,35 +26922,49 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Audio,
 	cr.plugins_.Browser,
 	cr.plugins_.Function,
-	cr.plugins_.Mouse,
 	cr.plugins_.Keyboard,
+	cr.plugins_.Mouse,
 	cr.plugins_.Sprite,
-	cr.plugins_.Spritefont2,
 	cr.plugins_.TiledBg,
+	cr.plugins_.Spritefont2,
 	cr.plugins_.Touch,
 	cr.behaviors.Pin,
 	cr.behaviors.Sin,
 	cr.behaviors.lunarray_LiteTween,
 	cr.behaviors.jumpthru,
 	cr.behaviors.solid,
-	cr.behaviors.Fade,
 	cr.behaviors.Platform,
-	cr.behaviors.Bullet,
 	cr.behaviors.scrollto,
+	cr.behaviors.Fade,
 	cr.behaviors.Flash,
+	cr.behaviors.Turret,
+	cr.behaviors.Bullet,
+	cr.system_object.prototype.cnds.IsGroupActive,
 	cr.system_object.prototype.cnds.OnLayoutStart,
 	cr.plugins_.Audio.prototype.cnds.IsTagPlaying,
 	cr.plugins_.Audio.prototype.acts.Play,
-	cr.plugins_.Touch.prototype.cnds.OnTapGestureObject,
-	cr.system_object.prototype.acts.GoToLayoutByName,
-	cr.system_object.prototype.acts.GoToLayout,
-	cr.system_object.prototype.cnds.IsGroupActive,
-	cr.system_object.prototype.acts.SetLayerVisible,
-	cr.system_object.prototype.acts.SetVar,
 	cr.system_object.prototype.cnds.CompareVar,
-	cr.system_object.prototype.cnds.Else,
-	cr.system_object.prototype.cnds.EveryTick,
+	cr.plugins_.Audio.prototype.acts.SetMuted,
 	cr.plugins_.Sprite.prototype.acts.SetAnimFrame,
+	cr.plugins_.Browser.prototype.acts.RequestFullScreen,
+	cr.plugins_.Browser.prototype.acts.CancelFullScreen,
+	cr.plugins_.Touch.prototype.cnds.OnTapGestureObject,
+	cr.system_object.prototype.acts.SetLayerVisible,
+	cr.plugins_.Function.prototype.acts.CallFunction,
+	cr.system_object.prototype.acts.SetVar,
+	cr.system_object.prototype.cnds.Else,
+	cr.plugins_.Browser.prototype.acts.Close,
+	cr.system_object.prototype.acts.GoToLayout,
+	cr.behaviors.Pin.prototype.acts.Pin,
+	cr.plugins_.Sprite.prototype.acts.SetY,
+	cr.plugins_.Function.prototype.cnds.OnFunction,
+	cr.behaviors.lunarray_LiteTween.prototype.acts.Start,
+	cr.behaviors.lunarray_LiteTween.prototype.acts.SetTarget,
+	cr.plugins_.Sprite.prototype.exps.X,
+	cr.system_object.prototype.cnds.LayerVisible,
+	cr.system_object.prototype.acts.Wait,
+	cr.behaviors.Sin.prototype.acts.SetActive,
+	cr.system_object.prototype.cnds.EveryTick,
 	cr.plugins_.Spritefont2.prototype.cnds.CompareInstanceVar,
 	cr.plugins_.Spritefont2.prototype.acts.SetText,
 	cr.plugins_.Sprite.prototype.cnds.IsAnimPlaying,
@@ -26538,43 +26976,30 @@ cr.getObjectRefTable = function () { return [
 	cr.plugins_.Keyboard.prototype.cnds.OnKey,
 	cr.plugins_.Sprite.prototype.acts.SetMirrored,
 	cr.plugins_.Sprite.prototype.cnds.IsOnScreen,
-	cr.behaviors.Sin.prototype.acts.SetActive,
+	cr.behaviors.Turret.prototype.acts.AddTarget,
+	cr.behaviors.Turret.prototype.cnds.OnShoot,
+	cr.plugins_.Sprite.prototype.acts.Spawn,
+	cr.behaviors.Bullet.prototype.acts.SetAngleOfMotion,
 	cr.plugins_.Sprite.prototype.cnds.CompareInstanceVar,
 	cr.behaviors.Platform.prototype.acts.SimulateControl,
 	cr.behaviors.Platform.prototype.cnds.IsByWall,
 	cr.plugins_.Sprite.prototype.acts.SetInstanceVar,
 	cr.plugins_.Sprite.prototype.cnds.OnCollision,
 	cr.plugins_.Sprite.prototype.acts.Destroy,
-	cr.system_object.prototype.acts.AddVar,
+	cr.plugins_.Sprite.prototype.acts.AddInstanceVar,
 	cr.behaviors.Flash.prototype.acts.Flash,
-	cr.plugins_.Sprite.prototype.acts.SubInstanceVar,
+	cr.system_object.prototype.acts.SubVar,
 	cr.plugins_.Sprite.prototype.cnds.IsMirrored,
-	cr.plugins_.Sprite.prototype.acts.Spawn,
-	cr.behaviors.Bullet.prototype.acts.SetAngleOfMotion,
+	cr.plugins_.Sprite.prototype.acts.SubInstanceVar,
+	cr.system_object.prototype.cnds.TriggerOnce,
 	cr.plugins_.Sprite.prototype.cnds.IsOutsideLayout,
 	cr.behaviors.Fade.prototype.acts.StartFade,
 	cr.behaviors.Platform.prototype.acts.SetEnabled,
-	cr.behaviors.Platform.prototype.acts.SetIgnoreInput,
-	cr.system_object.prototype.cnds.LayerVisible,
-	cr.system_object.prototype.cnds.TriggerOnce,
-	cr.plugins_.TiledBg.prototype.acts.Destroy,
-	cr.system_object.prototype.acts.Wait,
-	cr.system_object.prototype.acts.RestartLayout,
 	cr.behaviors.Bullet.prototype.acts.SetEnabled,
-	cr.plugins_.Sprite.prototype.acts.AddInstanceVar,
-	cr.plugins_.Audio.prototype.acts.SetMuted,
-	cr.plugins_.Browser.prototype.acts.RequestFullScreen,
-	cr.plugins_.Browser.prototype.acts.CancelFullScreen,
-	cr.plugins_.Touch.prototype.cnds.OnTouchObject,
-	cr.behaviors.Pin.prototype.acts.Pin,
-	cr.behaviors.lunarray_LiteTween.prototype.acts.SetTarget,
-	cr.behaviors.lunarray_LiteTween.prototype.acts.Start,
-	cr.plugins_.Sprite.prototype.exps.Y,
-	cr.plugins_.Sprite.prototype.acts.SetY,
-	cr.plugins_.Function.prototype.cnds.OnFunction,
-	cr.plugins_.Sprite.prototype.exps.X,
-	cr.plugins_.Function.prototype.acts.CallFunction,
-	cr.system_object.prototype.cnds.CompareTime,
-	cr.plugins_.Browser.prototype.acts.Close
+	cr.behaviors.Platform.prototype.acts.SetIgnoreInput,
+	cr.plugins_.TiledBg.prototype.acts.Destroy,
+	cr.system_object.prototype.acts.RestartLayout,
+	cr.system_object.prototype.acts.AddVar,
+	cr.plugins_.Touch.prototype.cnds.OnTouchObject
 ];};
 
